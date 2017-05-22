e7fe029501ea0e287b195572001265f9b30bee61
吴海东 <wu617509095@gmail.com>
2017-05-22 16:14:41 +0800

    User&Administrator

---
+++ Automatic-Course-Test-System/Automatic-Course-Test-System/Administrator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Automatic_Course_Test_System
+{
+    public partial class Administrator : Form
+    {
+        public Administrator()
+        {
+            InitializeComponent();
+        }
+    }
+}
