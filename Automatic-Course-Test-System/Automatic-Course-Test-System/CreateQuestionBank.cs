9d5c054c2eb048297e24f275eadd938d4b9ed99f
周方哲 <1041341764@qq.com>
2017-06-07 20:34:01 +0800

    新增测验

--- Automatic-Course-Test-System/Automatic-Course-Test-System/CreateQuestionBank.cs
+++ Automatic-Course-Test-System/Automatic-Course-Test-System/CreateQuestionBank.cs
@@ -32,10 +32,45 @@ namespace Automatic_Course_Test_System
             string KeMu = textBox1.Text.Trim();
             string CeYan = textBox2.Text.Trim();
             Close = false;
-            QuestionBankInformation f = new QuestionBankInformation(FatherForm);
-            f.getmessage_bank(zhanghao, KeMu, CeYan);
-            f.Show();
-            this.Close();
+
+            try
+            {
+                Encoding encoding = Encoding.GetEncoding("utf-8");
+                byte[] getWeatherUrl = encoding.GetBytes("http://1725r3a792.iask.in:28445/Server_Create.ashx?action=create&test=" + KeMu + "&specific_test=" + CeYan);
+                HttpWebRequest webReq = (HttpWebRequest)HttpWebRequest.Create("http://1725r3a792.iask.in:28445/Server_Create.ashx?action=create&test=" + KeMu + "&specific_test=" + CeYan);
+                webReq.Method = "post";
+                webReq.ContentType = "text/xml";
+
+                Stream outstream = webReq.GetRequestStream();
+                outstream.Write(getWeatherUrl, 0, getWeatherUrl.Length);
+                outstream.Flush();
+                outstream.Close();
+
+                webReq.Timeout = 2000;
+                HttpWebResponse webResp = (HttpWebResponse)webReq.GetResponse();
+                Stream stream = webResp.GetResponseStream();
+                StreamReader sr = new StreamReader(stream, encoding);
+                html = sr.ReadToEnd();
+                sr.Close();
+                stream.Close();
+            }
+            catch
+            {
+                MessageBox.Show("链接失败");
+            }
+            if(html == "1")
+            {
+                MessageBox.Show("创建测验成功，请加入具体题目");
+                QuestionBankInformation f = new QuestionBankInformation(FatherForm);
+                f.getmessage_bank(zhanghao, KeMu, CeYan);
+                f.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("创建测验失败,请修改测验信息");
+            }
+            
         }
         public void getmessage(string z)
         {
